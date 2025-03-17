49569163adfa7845eef4125f5bc7c8fe4bc3b0f5
yongjannes <2380444947@qq.com>
2025-03-17 16:26:14 +0800

    路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,14 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
+import AuthRoute from '@/components/AuthRoute'
 
 
 // 配置路由实例
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <AuthRoute><Layout /></AuthRoute>,
   },
   {
     path: '/login',
