10e7683cdb535892272c87d81bf73f2bd4f45794
yongjannes <2380444947@qq.com>
2025-03-17 16:35:04 +0800

    Layout页面结构和样式初始化

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import router from './router'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
