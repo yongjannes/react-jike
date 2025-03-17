87d5c22c81b86716a1b054ed4c2d8f65252d463a
yongjannes <2380444947@qq.com>
2025-03-17 17:15:31 +0800

    echarts图标初始化

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,36 @@
+import { useEffect, useRef } from 'react'
+import * as echarts from 'echarts'
+
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
+  const chartRef = useRef(null)
+  useEffect(() => {
+    // 1. 生成实例
+    const myChart = echarts.init(chartRef.current)
+    // 2. 准备图表参数
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'React', 'Angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    }
+    // 3. 渲染参数
+    myChart.setOption(option)
+  }, [])
+
+  return (
+    <div>
+      <div ref={chartRef} style={{ width: '400px', height: '300px' }} />
+    </div >
+  )
 }
 
-export default Home;
+export default Home
