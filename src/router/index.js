a63928f18ab1742d698dc83e1255ef172434e989
yongjannes <2380444947@qq.com>
2025-03-18 16:16:48 +0800

    l路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,16 @@ import { createBrowserRouter } from 'react-router-dom'
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
 import AuthRoute from '@/components/AuthRoute'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
+import { lazy,Suspense } from 'react'
 
+
+// import Home from '@/pages/Home'
+// import Article from '@/pages/Article'
+// import Publish from '@/pages/Publish'
+
+const Home = lazy(() => import('@/pages/Home'))
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
 
 // 配置路由实例
 const router = createBrowserRouter([
@@ -17,17 +23,30 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Home />
+          </Suspense>
+        )
       },
       {
         path: 'article',
-        element: <Article />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Article />
+          </Suspense>
+        )
       },
       {
         path: 'publish',
-        element: <Publish />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Publish />
+          </Suspense>
+        )
       },
-    ]
+    ],
   },
   {
     path: '/login',
