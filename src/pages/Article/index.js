14e810194b6234c7fc1a78b2904f8ca0f2616914
yongjannes <2380444947@qq.com>
2025-03-18 15:11:22 +0800

    文章列表：状态适配

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -18,6 +18,11 @@ const { RangePicker } = DatePicker
 const Article = () => {
     const { channels } = useChannel()
     // 准备列数据
+      // 定义状态枚举
+  const status = {
+    1: <Tag color='warning'>待审核</Tag>,
+    2: <Tag color='success'>审核通过</Tag>,
+  }
   const columns = [
     {
       title: '封面',
@@ -35,7 +40,11 @@ const Article = () => {
     {
       title: '状态',
       dataIndex: 'status',
-      render: data => <Tag color="green">审核通过</Tag>
+     // data - 后端返回的状态status 根据它做条件渲染
+      // data === 1 => 待审核
+      // data === 2 => 审核通过
+      render: data => status[data]
+
     },
     {
       title: '发布时间',
