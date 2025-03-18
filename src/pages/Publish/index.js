e12514fb91e32d5e09a595612c0ec9fdad04bccf
yongjannes <2380444947@qq.com>
2025-03-18 14:26:12 +0800

    发布文章：限制上传图片的数量

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -120,6 +120,7 @@ const Publish = () => {
                              showUploadList
                              action={'http://geek.itheima.net/v1_0/upload'}
                              onChange={onUploadChange}
+                             maxCount={imageType}
                         >
                             <div style={{ marginTop: 8 }}>
                                 <PlusOutlined />
